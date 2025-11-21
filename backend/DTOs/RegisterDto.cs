c3747b98a0077679e8be9a407883c3832846fba1
Roy <roywmraziv@icloud.com>
2025-11-21 14:00:28 -0600

    feat: create dtos

---
+++ backend/DTOs/RegisterDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FantasyDomainManager.DTOs;
+
+public class RegisterDto
+{
+    public required string Email { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Password { get; set; }
+}
